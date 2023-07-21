c62a2b6c459b511002cddb73934ac9531f69e2cd
Mike Jolley <mike.jolley@me.com>
2023-07-21 15:22:35 +0100

    Fix validateShippingCountry so error shows up even if it exists

--- assets/js/base/components/cart-checkout/address-form/validate-shipping-country.ts
+++ assets/js/base/components/cart-checkout/address-form/validate-shipping-country.ts
@@ -2,37 +2,41 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { FieldValidationStatus } from '@woocommerce/types';
 import { type ShippingAddress } from '@woocommerce/settings';
+import { select, dispatch } from '@wordpress/data';
+import { VALIDATION_STORE_KEY } from '@woocommerce/block-data';
 
 // If it's the shipping address form and the user starts entering address
 // values without having set the country first, show an error.
-const validateShippingCountry = (
-	values: ShippingAddress,
-	setValidationErrors: (
-		errors: Record< string, FieldValidationStatus >
-	) => void,
-	clearValidationError: ( error: string ) => void,
-	hasValidationError: boolean
-): void => {
+const validateShippingCountry = ( values: ShippingAddress ): void => {
 	const validationErrorId = 'shipping_country';
+	const hasValidationError =
+		select( VALIDATION_STORE_KEY ).getValidationError( validationErrorId );
 	if (
-		! hasValidationError &&
 		! values.country &&
 		( values.city || values.state || values.postcode )
 	) {
-		setValidationErrors( {
-			[ validationErrorId ]: {
-				message: __(
-					'Please select a country to calculate rates.',
-					'woo-gutenberg-products-block'
-				),
-				hidden: false,
-			},
-		} );
+		if ( hasValidationError ) {
+			dispatch( VALIDATION_STORE_KEY ).showValidationError(
+				validationErrorId
+			);
+		} else {
+			dispatch( VALIDATION_STORE_KEY ).setValidationErrors( {
+				[ validationErrorId ]: {
+					message: __(
+						'Please select your country',
+						'woo-gutenberg-products-block'
+					),
+					hidden: false,
+				},
+			} );
+		}
 	}
+
 	if ( hasValidationError && values.country ) {
-		clearValidationError( validationErrorId );
+		dispatch( VALIDATION_STORE_KEY ).clearValidationError(
+			validationErrorId
+		);
 	}
 };
 
