9fe69b89358537e3f05d2fb423d8fb79fe2d52b9
Thomas Roberts <thomas.roberts@automattic.com>
2023-07-28 09:30:19 +0100

    Add class to handle selecting gift wrapping

---
+++ src/StoreApi/Routes/V1/CartSelectGiftWrapping.php
@@ -0,0 +1,70 @@
+<?php
+namespace Automattic\WooCommerce\StoreApi\Routes\V1;
+
+use Automattic\WooCommerce\StoreApi\Exceptions\RouteException;
+use Automattic\WooCommerce\StoreApi\Utilities\CartController;
+
+/**
+ * CartRemoveCoupon class.
+ */
+class CartSelectGiftWrapping extends AbstractCartRoute {
+	/**
+	 * The route identifier.
+	 *
+	 * @var string
+	 */
+	const IDENTIFIER = 'cart-select-gift-wrapping';
+
+	/**
+	 * Get the path of this REST route.
+	 *
+	 * @return string
+	 */
+	public function get_path() {
+		return '/cart/select-gift-wrapping';
+	}
+
+	/**
+	 * Get method arguments for this REST route.
+	 *
+	 * @return array An array of endpoints.
+	 */
+	public function get_args() {
+		return [
+			[
+				'methods'             => \WP_REST_Server::CREATABLE,
+				'callback'            => [ $this, 'get_response' ],
+				'permission_callback' => '__return_true',
+				'args'                => [
+					'gift_wrapping' => [
+						'description' => __( 'Whether the order should be gift-wrapped.', 'woo-gutenberg-products-block' ),
+						'type'        => 'boolean',
+					],
+				],
+			],
+			'schema'      => [ $this->schema, 'get_public_item_schema' ],
+			'allow_batch' => [ 'v1' => true ],
+		];
+	}
+
+	/**
+	 * Handle the request and return a valid response for this endpoint.
+	 *
+	 * @throws RouteException On error.
+	 * @param \WP_REST_Request $request Request object.
+	 * @return \WP_REST_Response
+	 */
+	protected function get_route_post_response( \WP_REST_Request $request ) {
+		$cart_controller             = new CartController();
+		$gift_wrapping_session_value = (bool) wc()->session->get( 'wc_blocks_gift_wrapping' );
+
+		if ( isset( $request['gift_wrapping'] ) && $request['gift_wrapping'] ) {
+			wc()->session->set( 'wc_blocks_gift_wrapping', (bool) $request['gift_wrapping'] );
+		}
+		if ( true === $gift_wrapping_session_value ) {
+			wc()->session->set( 'wc_blocks_gift_wrapping', false );
+		}
+		$cart_controller->calculate_totals();
+		return rest_ensure_response( $this->schema->get_item_response( $cart_controller->get_cart_instance() ) );
+	}
+}
