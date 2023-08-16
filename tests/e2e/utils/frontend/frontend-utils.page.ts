5157151911b0fceb8c74a59432901d1ed5460048
Saad Tarhi <saad.trh@gmail.com>
2023-08-16 07:25:03 +0100

    Reset My account page title

--- tests/e2e/utils/frontend/frontend-utils.page.ts
+++ tests/e2e/utils/frontend/frontend-utils.page.ts
@@ -136,9 +136,7 @@ export class FrontendUtils {
 
 	async isLoggedIn() {
 		await this.gotoMyAccount();
-		await expect( this.page ).toHaveTitle(
-			/My Account – WooCommerce Blocks E2E Test Suite/
-		);
+		await expect( this.page ).toHaveTitle( /My account/ );
 		const loginForm = this.page.locator( 'form.woocommerce-form-login' );
 
 		return ! loginForm;
@@ -146,9 +144,7 @@ export class FrontendUtils {
 
 	async login() {
 		await this.gotoMyAccount();
-		await expect( this.page ).toHaveTitle(
-			/My Account – WooCommerce Blocks E2E Test Suite/
-		);
+		await expect( this.page ).toHaveTitle( /My account/ );
 		await this.page
 			.locator( 'input[name="username"]' )
 			.fill( customer.username );
@@ -162,9 +158,7 @@ export class FrontendUtils {
 
 	async logout() {
 		await this.gotoMyAccount();
-		await expect( this.page ).toHaveTitle(
-			/My Account – WooCommerce Blocks E2E Test Suite/
-		);
+		await expect( this.page ).toHaveTitle( /My account/ );
 		await this.page.locator( 'text=Log out' ).click();
 	}
 }
