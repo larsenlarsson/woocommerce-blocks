70067178a5e5f24102a45135f6aac285d9a59373
Saad Tarhi <saad.trh@gmail.com>
2023-08-18 12:57:37 +0100

    Update testing cases

--- tests/e2e/tests/checkout/constants.ts
+++ tests/e2e/tests/checkout/constants.ts
@@ -1,2 +1,4 @@
 export const SIMPLE_VIRTUAL_PRODUCT_NAME = 'Album';
 export const SIMPLE_PHYSICAL_PRODUCT_NAME = 'Beanie';
+export const FREE_SHIPPING_NAME = 'Free shipping';
+export const FREE_SHIPPING_PRICE = '$0.00';
